c0a3733312493aedb9a4f1a834fe0510a8cd334f
Matthew <matthew.g.fogarty@gmail.com>
2021-04-22 14:56:53 +0100

    "Second Commit"

--- main.py
+++ main.py
@@ -15,3 +15,18 @@ print(RetailPrices.head())
 #check for missing data
 print(PrimaryEnergy.isnull().sum())
 print(RetailPrices.isnull().sum())
+
+#check all columns are accounted for
+print(PrimaryEnergy.shape)
+print(RetailPrices.shape)
+
+#merging the two data sets
+EP = PrimaryEnergy.merge(RetailPrices, on = 'YYYYMM', suffixes=('_Energy','_Prices'))
+print(EP.columns)
+
+#creating a pivot of the data
+pivot_Energy = EP.pivot_table(values=["Value_Energy"], index=["YYYYMM"], columns=["Description_Energy"], aggfunc='sum')
+print(pivot_Energy)
+
+pivot_Prices = EP.pivot_table(values=["Value_Prices"], index=["YYYYMM"], columns=["Description_Prices"], aggfunc='sum')
+print(pivot_Prices)
